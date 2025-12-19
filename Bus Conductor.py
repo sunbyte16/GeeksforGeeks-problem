43163ca64ac9ba461ce2757f4a8701b131071214
𝙎𝙪𝙣𝙞𝙡 𝙎𝙝𝙖𝙧𝙢𝙖 <ji2796709@gmail.com>
2025-12-19 06:08:40 +0530

    Create Bus Conductor.py

---
+++ Bus Conductor.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findMoves(self, chairs, passengers):
+        chairs.sort()
+        passengers.sort()
+        moves = 0
+        for c, p in zip(chairs, passengers):
+            moves += abs(c - p)
+        return moves
