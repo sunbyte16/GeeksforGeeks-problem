2926f84a48ed184319816eb4e916b4ab47bc6d50
𝙎𝙪𝙣𝙞𝙡 𝙎𝙝𝙖𝙧𝙢𝙖 <ji2796709@gmail.com>
2026-01-20 08:22:03 +0530

    Create Implement UNDO & REDO.py

---
+++ Implement UNDO & REDO.py
@@ -0,0 +1,19 @@
+class Solution:
+    def __init__(self):
+        self.doc = []
+        self.redo_stack = []
+
+    def append(self, x):
+        self.doc.append(x)
+        self.redo_stack.clear()
+
+    def undo(self):
+        if self.doc:
+            self.redo_stack.append(self.doc.pop())
+
+    def redo(self):
+        if self.redo_stack:
+            self.doc.append(self.redo_stack.pop())
+
+    def read(self):
+        return "".join(self.doc)
