7a63db1129acf97770a5754f359d59955e771761
𝙎𝙪𝙣𝙞𝙡 𝙎𝙝𝙖𝙧𝙢𝙖 <ji2796709@gmail.com>
2026-03-11 08:42:04 +0530

    Create Sum of subarray minimums.py

---
+++ Sum of subarray minimums.py
@@ -0,0 +1,40 @@
+class Solution:
+    def sumSubMins(self, arr):
+        n = len(arr)
+        
+        stack = []
+        left = [0] * n
+        right = [0] * n
+        
+        # Previous Smaller Element
+        for i in range(n):
+            while stack and arr[stack[-1]] > arr[i]:
+                stack.pop()
+            
+            if stack:
+                left[i] = i - stack[-1]
+            else:
+                left[i] = i + 1
+            
+            stack.append(i)
+        
+        stack.clear()
+        
+        # Next Smaller Element
+        for i in range(n - 1, -1, -1):
+            while stack and arr[stack[-1]] >= arr[i]:
+                stack.pop()
+            
+            if stack:
+                right[i] = stack[-1] - i
+            else:
+                right[i] = n - i
+            
+            stack.append(i)
+        
+        ans = 0
+        
+        for i in range(n):
+            ans += arr[i] * left[i] * right[i]
+        
+        return ans
