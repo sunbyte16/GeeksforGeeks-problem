8aac6f39de8abe3b0aa27da3440f3f1d4061dc3b
𝙎𝙪𝙣𝙞𝙡 𝙎𝙝𝙖𝙧𝙢𝙖 <ji2796709@gmail.com>
2026-04-01 07:35:55 +0530

    Implement countStrings method to count valid strings

---
+++ Consecutive 1's not allowed.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countStrings(self, n):
+        if n == 1:
+            return 2
+        
+        a, b = 2, 3  # dp[1], dp[2]
+        
+        for _ in range(3, n + 1):
+            a, b = b, a + b
+        
+        return b
