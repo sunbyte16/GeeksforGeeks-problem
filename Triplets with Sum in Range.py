97d66911b8db1577d9121471380f343457b0e818
𝙎𝙪𝙣𝙞𝙡 𝙎𝙝𝙖𝙧𝙢𝙖 <ji2796709@gmail.com>
2026-08-19 07:39:58 +0530

    Today Commits
    Implement function to count triplets with sum in a given range.

---
+++ Triplets with Sum in Range.py
@@ -0,0 +1,13 @@
+from bisect import bisect_left, bisect_right
+        arr.sort()
+        n = len(arr)
+        ans = 0
+        for i in range(n):
+            for j in range(i+1, n):
+                s = arr[i]+arr[j]
+                k1 = bisect_left(arr, l-s)
+                k1 = max(k1, j+1)
+                k2 = bisect_right(arr, r-s)
+                #k2 = min(k2, n)
+                ans += max(0, k2-k1)
+        return ans
