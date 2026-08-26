fa4c2934df3d3222b6cfe9e25ca0c0be335e7aed
𝙎𝙪𝙣𝙞𝙡 𝙎𝙝𝙖𝙧𝙢𝙖 <ji2796709@gmail.com>
2026-08-27 04:23:44 +0530

    Create Largest Rectangle with Column Swaps.py

---
+++ Largest Rectangle with Column Swaps.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxArea(self, mat: list[list[int]]) -> int:
+        # code here
+        n = len(mat[0])
+        heights = [0] * n
+        max_area = 0
+        for row in mat:
+            for i in range(n):
+                heights[i] = heights[i] + 1 if row[i] else 0
+            sheights = sorted(filter(None, heights), reverse=True)
+            for j in range(len(sheights)):
+                max_area = max(max_area, sheights[j] * (j + 1))
+        return max_area
